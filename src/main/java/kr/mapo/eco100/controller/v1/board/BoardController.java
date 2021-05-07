b5e4b315ef19c0641efbb1b23af1bdc4cdcbcb6a
SGKwon95 <ssunder7@naver.com>
2021-05-07 13:43:44 +0900

    feat : 이미지 등록 기능 추가

--- src/main/java/kr/mapo/eco100/controller/v1/board/BoardController.java
+++ src/main/java/kr/mapo/eco100/controller/v1/board/BoardController.java
@@ -4,13 +4,13 @@ import kr.mapo.eco100.controller.v1.board.dto.BoardDto;
 import kr.mapo.eco100.controller.v1.board.dto.CreateRequest;
 import kr.mapo.eco100.controller.v1.board.dto.IncreaseLikesRequest;
 import kr.mapo.eco100.controller.v1.board.dto.UpdateRequest;
-import kr.mapo.eco100.error.BoardNotFoundException;
 import kr.mapo.eco100.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -23,26 +23,32 @@ public class BoardController {
 
     @PostMapping("/board/create")
     public ResponseEntity<BoardDto> create(@RequestBody @Valid CreateRequest createRequest) {
+
+        return ResponseEntity.ok(new BoardDto(boardService.create(createRequest)));
+    }
+
+    @PostMapping(value = "/board/create/image", consumes = { "multipart/form-data" })
+    public ResponseEntity<BoardDto> createWithImage(MultipartHttpServletRequest request) throws IOException {
         
-        return ResponseEntity.ok(
-            new BoardDto(boardService.create(createRequest))
-        );
+        @Valid
+        CreateRequest createRequest = CreateRequest.builder()
+                .userId(Long.parseLong(request.getAttribute("userId").toString()))
+                .title(request.getAttribute("title").toString()).contents(request.getAttribute("contents").toString())
+                .build();
+
+        return ResponseEntity.ok(new BoardDto(boardService.createWithImage(createRequest, request.getFile("image"))));
     }
 
     @GetMapping("/board/read/{id}")
     public ResponseEntity<BoardDto> read(@PathVariable(value = "id") Long boardId) {
-        
-        return ResponseEntity.ok(
-            new BoardDto(boardService.read(boardId))
-        );
+
+        return ResponseEntity.ok(new BoardDto(boardService.read(boardId)));
     }
 
     @GetMapping("/board/{current}")
     public ResponseEntity<List<BoardDto>> getRecentBoards(@PathVariable(value = "current") int currentPage) {
 
-        return ResponseEntity.ok(
-            boardService.getRecentBoards(currentPage)
-        );
+        return ResponseEntity.ok(boardService.getRecentBoards(currentPage));
     }
 
     @PutMapping("/board/update")
@@ -55,13 +61,11 @@ public class BoardController {
     @PatchMapping("/board/likes")
     public ResponseEntity<Boolean> increaseLikes(@RequestBody @Valid IncreaseLikesRequest increaseLikesRequest) {
 
-        return ResponseEntity.ok(
-            boardService.increaseLikes(increaseLikesRequest)
-        );
+        return ResponseEntity.ok(boardService.increaseLikes(increaseLikesRequest));
     }
 
     @DeleteMapping("/board/delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable(value="id") Long boardId) {
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long boardId) {
         boardService.delete(boardId);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/kr/mapo/eco100/service/BoardService.java
+++ src/main/java/kr/mapo/eco100/service/BoardService.java
@@ -1,10 +1,15 @@
 package kr.mapo.eco100.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.mapo.eco100.controller.v1.board.dto.BoardDto;
 import kr.mapo.eco100.controller.v1.board.dto.CreateRequest;
@@ -29,58 +34,80 @@ public class BoardService {
     private final LikesService likesService;
 
     public Board create(CreateRequest createRequest) {
-        
+
         User user = userRepository.findById(createRequest.getUserId())
                 .orElseThrow(() -> new UserNotFoundException("해당 유저가 존재하지 않습니다"));
 
+        return boardRepository.save(Board.builder().title(createRequest.getTitle())
+                .contents(createRequest.getContents()).imageUrl(createRequest.getImageUrl()).user(user).build());
+    }
+
+    public Board createWithImage(CreateRequest createRequest, MultipartFile file) throws IOException {
+
+        User user = userRepository.findById(createRequest.getUserId())
+                .orElseThrow(() -> new UserNotFoundException("해당 유저가 존재하지 않습니다"));
+
+        String fileLocation = "./images/board/"; // 저장할 경로를 지정한다.
+        FileOutputStream fos = null;
+        try {
+
+            File dir = new File(fileLocation);
+            dir.mkdirs();// 저장할 경로에 지정된 폴더가 없으면 생성한다. 있으면 아무일도 일어나지 않는다.
+
+            int start = file.getOriginalFilename().lastIndexOf('.');
+            String extension = file.getOriginalFilename().substring(start);// 파일 이름의 확장자만 추출한다.
+            String filename = UUID.randomUUID().toString() + extension;
+
+            File imgfile = new File(fileLocation + filename);
+            fos = new FileOutputStream(imgfile);// fileoutputstream은 new로 만들 시 파일을 생성한다.
+            fos.write(file.getBytes());// 전송된 이미지를 파일에 저장한다.
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fos.close();
+        }
+        
         return boardRepository.save(
-                Board.builder()
-                        .title(createRequest.getTitle())
-                        .contents(createRequest.getContents())
-                        .imageUrl(createRequest.getImageUrl())
-                        .user(user)
-                        .build()
+            Board.builder()
+                .title(createRequest.getTitle())
+                .contents(createRequest.getContents())
+                .imageUrl(createRequest.getImageUrl())
+                .user(user)
+                .build()
         );
     }
 
     public Board read(Long boardId) {
-        return boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException("조회한 아이디의 게시글이 없습니다"));
+        return boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("조회한 아이디의 게시글이 없습니다"));
     }
 
     public List<BoardDto> getRecentBoards(int currentPage) {
-        Page<Board> page = boardRepository.findAll(PageRequest.of(currentPage, currentPage + 5, Sort.Direction.DESC, "created_at"));
-        return page.getContent().stream()
-                .map(BoardDto::new)
-                .collect(Collectors.toList());
+        Page<Board> page = boardRepository
+                .findAll(PageRequest.of(currentPage, currentPage + 5, Sort.Direction.DESC, "boardId"));
+        return page.getContent().stream().map(BoardDto::new).collect(Collectors.toList());
     }
 
     public void update(UpdateRequest updateRequest) {
-        boardRepository.findById(updateRequest.getBoardId())
-                .map(board -> {
-                    board.setTitle(updateRequest.getTitle());
-                    board.setContents(updateRequest.getContents());
-                    board.setImageUrl(updateRequest.getImageUrl());
-                    return boardRepository.save(board);
-                })
-                .orElseThrow(() -> new BoardNotFoundException("조회한 게시글 아이디의 게시글이 없습니다"));
+        boardRepository.findById(updateRequest.getBoardId()).map(board -> {
+            board.setTitle(updateRequest.getTitle());
+            board.setContents(updateRequest.getContents());
+            board.setImageUrl(updateRequest.getImageUrl());
+            return boardRepository.save(board);
+        }).orElseThrow(() -> new BoardNotFoundException("조회한 게시글 아이디의 게시글이 없습니다"));
     }
 
     public void delete(Long boardId) {
         boardRepository.delete(
-                boardRepository.findById(boardId)
-                        .orElseThrow(() -> new BoardNotFoundException("삭제할 아이디의 게시글이 없습니다"))
-        );
+                boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("삭제할 아이디의 게시글이 없습니다")));
     }
 
     public Boolean increaseLikes(IncreaseLikesRequest increaseLikesRequest) {
         boolean result = likesService.save(increaseLikesRequest);
-        boardRepository.findById(increaseLikesRequest.getBoardId())
-                .map(board -> {
-                    board.setLikes(board.getLikes() + (result ? 1 : -1));
-                    return boardRepository.save(board);
-                })
-                .orElseThrow(() -> new BoardNotFoundException("조회한 아이디의 게시글이 없습니다"));
+        boardRepository.findById(increaseLikesRequest.getBoardId()).map(board -> {
+            board.setLikes(board.getLikes() + (result ? 1 : -1));
+            return boardRepository.save(board);
+        }).orElseThrow(() -> new BoardNotFoundException("조회한 아이디의 게시글이 없습니다"));
         return result;
     }
 }
