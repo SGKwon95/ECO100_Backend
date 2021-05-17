468d351ec17d65e16260638501ec5f373abb5df9
SGKwon95 <ssunder7@naver.com>
2021-05-17 16:40:50 +0900

    findByUserAndChallenge 추가

--- src/main/java/kr/mapo/eco100/repository/challenge/ChallengeUserRespoitory.java
+++ src/main/java/kr/mapo/eco100/repository/challenge/ChallengeUserRespoitory.java
@@ -3,6 +3,8 @@ package kr.mapo.eco100.repository.challenge;
 import java.util.List;
 import java.util.Optional;
 
+import kr.mapo.eco100.entity.Challenge;
+import kr.mapo.eco100.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import kr.mapo.eco100.entity.ChallengeUser;
 
 @Repository
 public interface ChallengeUserRespoitory extends JpaRepository<ChallengeUser, Long> {
-    Optional<List<ChallengeUser>> findByuserIdAndchallengeIdIn(Long userId, List<Long> challengeIds);
+    Optional<List<ChallengeUser>> findByUserAndChallengeIn(User user, List<Challenge> challenges);
+    Optional<ChallengeUser> findByUserAndChallenge(User user, Challenge challenge);
 }
