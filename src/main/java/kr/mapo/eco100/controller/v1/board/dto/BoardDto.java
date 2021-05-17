618e36443d9291edd9fab0355e25c11aa4fd27d0
SGKwon95 <ssunder7@naver.com>
2021-05-17 11:13:43 +0900

    댓글 null일경우 empty list 반환

--- src/main/java/kr/mapo/eco100/controller/v1/board/dto/BoardDto.java
+++ src/main/java/kr/mapo/eco100/controller/v1/board/dto/BoardDto.java
@@ -1,15 +1,18 @@
 package kr.mapo.eco100.controller.v1.board.dto;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+
 import kr.mapo.eco100.entity.Board;
+import kr.mapo.eco100.entity.Comment;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
 @ToString
-@Slf4j
 public class BoardDto {
 
     private Long board_id;
@@ -22,19 +25,32 @@ public class BoardDto {
 
     private String image_url;
 
+    private List<Comment> comments;
+
     private Integer comments_cnt;
 
     private Integer likes_cnt;
 
-    public BoardDto(Board board) {
-        //copyProperties(source,this);
-        log.info(board.toString());
+    private Boolean can_click_likes;
+
+    private String date;
+
+    public BoardDto(Board board, boolean can_click_likes){
+        
         this.board_id = board.getBoardId();
         this.user_nickname = board.getUser().getNickname();
         this.title = board.getTitle();
         this.contents = board.getContents();
         this.image_url = board.getImageUrl();
-        this.comments_cnt = 0;
-        this.likes_cnt = 0;
+        if (board.getComments() != null) {
+            this.comments = board.getComments();
+            this.comments_cnt = this.comments.size();
+        } else {
+            this.comments = Collections.emptyList();
+            this.comments_cnt = 0;
+        }
+        this.likes_cnt = board.getLikes();
+        this.can_click_likes = can_click_likes;
+        this.date = (board.getModifiedDate() != null)? board.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) : null;
     }
 }
