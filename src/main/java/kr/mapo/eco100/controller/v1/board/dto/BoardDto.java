61bd751ffe84bee3997932fb32adf3cee4da4e77
SGKwon95 <ssunder7@naver.com>
2021-05-20 11:40:04 +0900

    field 변수명 _를 사용하지 않도록 변경

--- src/main/java/kr/mapo/eco100/controller/v1/board/dto/BoardDto.java
+++ src/main/java/kr/mapo/eco100/controller/v1/board/dto/BoardDto.java
@@ -3,9 +3,10 @@ package kr.mapo.eco100.controller.v1.board.dto;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import kr.mapo.eco100.controller.v1.comment.dto.CommentDto;
 import kr.mapo.eco100.entity.Board;
-import kr.mapo.eco100.entity.Comment;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,42 +16,45 @@ import lombok.ToString;
 @ToString
 public class BoardDto {
 
-    private Long board_id;
+    private Long boardId;
 
-    private String user_nickname;
+    private Long userId;
+
+    private String nickname;
 
     private String title;
 
     private String contents;
 
-    private String image_url;
+    private String imageUrl;
 
-    private List<Comment> comments;
+    private List<CommentDto> comments;
 
-    private Integer comments_cnt;
+    private Integer commentsCnt;
 
-    private Integer likes_cnt;
+    private Integer likesCnt;
 
-    private Boolean can_click_likes;
+    private Boolean canClickLikes;
 
     private String date;
 
     public BoardDto(Board board, boolean can_click_likes){
         
-        this.board_id = board.getBoardId();
-        this.user_nickname = board.getUser().getNickname();
+        this.boardId = board.getBoardId();
+        this.userId = board.getUser().getUserId();
+        this.nickname = board.getUser().getNickname();
         this.title = board.getTitle();
         this.contents = board.getContents();
-        this.image_url = board.getImageUrl();
+        this.imageUrl = board.getImageUrl();
         if (board.getComments() != null) {
-            this.comments = board.getComments();
-            this.comments_cnt = this.comments.size();
+            this.comments = board.getComments().stream().map(CommentDto::new).collect(Collectors.toList());
+            this.commentsCnt = this.comments.size();
         } else {
             this.comments = Collections.emptyList();
-            this.comments_cnt = 0;
+            this.commentsCnt = 0;
         }
-        this.likes_cnt = board.getLikes();
-        this.can_click_likes = can_click_likes;
+        this.likesCnt = board.getLikes();
+        this.canClickLikes = can_click_likes;
         this.date = (board.getModifiedDate() != null)? board.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) : null;
     }
 }
