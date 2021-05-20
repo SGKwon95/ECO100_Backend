b12290297b46bef509aaa9e12bac99a02f2e5ac6
SGKwon95 <ssunder7@naver.com>
2021-05-20 11:45:04 +0900

    댓글 DTO 추가

---
+++ src/main/java/kr/mapo/eco100/controller/v1/comment/dto/CommentDto.java
@@ -0,0 +1,34 @@
+package kr.mapo.eco100.controller.v1.comment.dto;
+
+import java.time.format.DateTimeFormatter;
+
+import kr.mapo.eco100.entity.Comment;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CommentDto {
+
+    private Long commentId;
+
+    private String writer;
+
+    private String contents;
+
+    private String date;
+
+    public CommentDto(Comment comment){
+        this.commentId = comment.getCommentId();
+        this.writer = comment.getUser().getNickname();
+        this.contents = comment.getContents();
+        this.date = (comment.getModifiedDate() != null)? comment.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) : null;
+    }
+}
+
+
+    
+
+    
