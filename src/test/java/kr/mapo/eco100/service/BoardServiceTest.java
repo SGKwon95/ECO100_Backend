121b85c415493f4ad43130ad8e7ce7fc0cc6b69d
SGKwon95 <ssunder7@naver.com>
2021-06-28 05:30:06 +0900

    chore : 사용하지 않는 library 삭제

--- src/test/java/kr/mapo/eco100/service/BoardServiceTest.java
+++ src/test/java/kr/mapo/eco100/service/BoardServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import kr.mapo.eco100.entity.Board;
